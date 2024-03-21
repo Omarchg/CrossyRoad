8033a367a6841aa5b6c6bd9fbbd21ab2aee677d0
Omarchg <omarchaglez@gmail.com>
2024-03-21 10:11:02 +0000

    MovimientoPerfecto

---
+++ Assets/Scripts/TriggerExit.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerExit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        
+    }
+    
+
+    
+
+
+}
